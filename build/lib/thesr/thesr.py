82ef22432540810038723e98ad5f0c9f52f336c8
treatmesubj <jrock4503@hotmail.com>
2023-04-23 10:41:23 -0500

    IPV4 for fricking Webster

--- build/lib/thesr/thesr.py
+++ build/lib/thesr/thesr.py
@@ -13,15 +13,17 @@ import socket
 import requests.packages.urllib3.util.connection as urllib3_cn
 
 
-def _allowed_gai_family():
-    # https://github.com/shazow/urllib3/blob/master/urllib3/util/connection.py
+def allowed_gai_family():
+    """
+     https://github.com/shazow/urllib3/blob/master/urllib3/util/connection.py
+    """
     family = socket.AF_INET
     #if urllib3_cn.HAS_IPV6:
     #    family = socket.AF_INET6 # force ipv6 only if it is available
     return family
 
 
-urllib3_cn.allowed_gai_family = _allowed_gai_family
+urllib3_cn.allowed_gai_family = allowed_gai_family
 
 
 def get_random_word():
