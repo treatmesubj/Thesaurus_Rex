2c588ab58249931159be637fc4eccf4e1e19f2f9
treatmesubj <jrock4503@hotmail.com>
2023-04-23 19:03:10 -0500

    getting there

--- docker/README.md
+++ docker/README.md
@@ -1,3 +1,6 @@
 1. [Dockerfile](Dockerfile)
 2. $ `docker build -t webserv_img .`
 3. $ `docker run -itd -p 8000:80 --name webserv webserv_img`
+
+
+- `flask run -p 8000`
--- docker/src/app.py
+++ docker/src/app.py
@@ -14,29 +14,43 @@ from thesr.thesr import *
 app = Flask(__name__)
 
 
-# decorator for homepage
-@app.route("/", methods=['GET', 'POST'])
-def index():
-    url_for('static', filename='favicon.css')
-    url_for('static', filename='style.css')
-    url_for('static', filename='avoid_space.js')
-    if request.method == 'GET':
+@app.route("/", methods=["GET", "POST"])
+def index(error=None):
+    url_for("static", filename="favicon.css")
+    url_for("static", filename="style.css")
+    url_for("static", filename="avoid_space.js")
+    if request.method == "GET":
         return render_template("index.html")
     else:
-        assert request.method == 'POST'
-        print(request.form)
+        assert request.method == "POST"
+        if request.form["word"] == "":
+            return render_template("index.html", error="No word provided")
 
-        if request.form['word'] != '':
-            print(f"word: {request.form['word']}")
-            #thesr_word = Word(word)
-        if 'define' in request.form.keys():
-            print("define")
-        if 'etymology' in request.form.keys():
-            print("etymology")
-        if 'antonyms' in request.form.keys():
+        return thesr(request_form=request.form)
 
-            print("antonyms")
-        return render_template("index.html")
+
+@app.route("/thesr")
+def thesr(request_form):
+    thesr_word = Word(request_form["word"])
+    word_spelling = thesr_word.spelling
+    # definitions
+    if "define" in request_form.keys():
+        thesr_word.webster_homonyms = get_defs("word")
+    # etymology
+    if "etymology" in request_form.keys():
+        thesr_word.etymology = get_etymology("word")
+    # antonyms
+    if "antonyms" in request_form.keys():
+        pass
+
+    return render_template(
+        "thesr.html",
+        word_spelling=word_spelling,
+        # synonyms=synonyms,
+        # definitions=definitions,
+        # etymology=etymology,
+        # antonyms=antonyms,
+    )
 
 
 if __name__ == "__main__":
